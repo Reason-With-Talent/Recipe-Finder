7f4ed2a6edc3c8cb4ca3d7afc7731653a61c62b3
Reason-With-Talent <oluwatobibalogun2@gmai.com>
2024-02-04 01:44:42 -0500

    content update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,33 @@
 import { AiOutlineUser, AiOutlineSearch, AiOutlineHeart, AiOutlineMenu } from "react-icons/ai"
+import { FaTimes } from "react-icons/fa"
 import logo from "../assets/IMG/Logo (20).svg"
+import { useState } from "react";
 const Header = () => {
+    const [open, setOpen] = useState(true);
+    const handleOpen = () => {
+        if (open) {
+            setOpen(false)
+        }
+        else {
+            setOpen(true)
+        }
+
+    };
     return (
         <>
             <header className="flex md:justify-between bg-[#f6b100] py-4 md:px-[100px] px-5 items-center">
-                <AiOutlineMenu className="md:hidden text-[20px]" />
+                {open ? <AiOutlineMenu onClick={handleOpen} className="md:hidden text-[20px]" /> :
+                    <div className="relative">
+                        <FaTimes onClick={handleOpen} className="md:hidden" />
+                        <div className="flex flex-col bg-[#f6b100] absolute w-[100px] h-[200px] rounded-lg">
+                            <span className="text-[#FFFFFF] p-[10px] font-medium text-[15px]">Home</span>
+                            <span className="text-[#FFFFFF] p-[10px] font-medium text-[15px]">About</span>
+                            <span className="text-[#FFFFFF] p-[10px] font-medium text-[15px]">Contact</span>
+                            <span className="text-[#FFFFFF] p-[10px] font-medium text-[15px]">FAQ</span>
+                        </div>
+
+                    </div>
+                }
                 <div className="w-fit self-center md:self-start m-auto md:m-0">
                     <img src={logo} className="img-fluid w-[200px]" />
                 </div>
